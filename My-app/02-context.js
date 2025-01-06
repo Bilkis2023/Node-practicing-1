18e7907e805894b49a72fc2d0062781fdfd43b65
mst <bilkis.naher@yahoo.co.uk>
2025-01-06 23:55:20 +0000

    add context file and node's object, function

---
+++ My-app/02-context.js
@@ -0,0 +1,28 @@
+// Depending on the environment `setTimeout ` is called in, it may be refer to one of tow objects
+// In the browser `setTimeout` is property of `window` object
+// In node, it belongs to a special "Timeout" object
+
+var person = {
+    name: "Hodor",
+    saySomething: function(){
+        console.log(this.name + "is thinking...");
+        setTimeout(function(){
+            console.log(this.name + "!");
+        }, 1000);
+    }
+};
+
+// person.saySomething();  //prints "Hodor is thinking...
+// prints "undefined!" 100ms later
+
+// Arrow function bind the `this` keyword to the object it's created inside of
+// i.e. whatever `this` is where it's created
+var person = {
+    name: "Hodor",
+    saySomething: function(){
+        console.log(this.name + "is thinking...");
+        setTimeout(()=> console.log(this.name + "!"), 1000); 
+    }
+}
+
+person.saySomething();
