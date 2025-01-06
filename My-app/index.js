1c71911926300d5da529d3436fbf30c3b003be06
mst <bilkis.naher@yahoo.co.uk>
2025-01-06 17:11:46 +0000

    add arrow function's syntex es6

--- My-app/index.js
+++ My-app/index.js
@@ -4,4 +4,26 @@
 //     console.log(this);
 // }) ();
 
-console.log("Hellooo, Node!")
+// console.log("Hellooo, Node!")
+
+// All of the `createGreeting ` functions are equivalent:
+var createGreeting = function(message, name) {
+    return message + "," + name + "!";
+};
+
+// We can safely swap out function expressions with arrow functions most of the time
+var createGreeting = (message, name) => {
+    return message + "," + name + "!";
+};
+
+// If the arrow function contains only one expression, we can omit the curly braces and auto return it
+var createGreeting = (message, name) => message + "," + name + "!";
+
+// If an arrow function has only one parameter, we can omit the parens () around the single parameter
+var greet = greeting => console.log(greeting);
+
+// We call arrow functions as we call regular functions
+var greeting = createGreeting("Hello", "Bilkis");
+
+// Logs "Hello, Bilkis!";
+greet(greeting);
