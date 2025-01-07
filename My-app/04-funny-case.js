5853770eb19c0b77a2f19802e0cbd7700ba0c0c1
mst <bilkis.naher@yahoo.co.uk>
2025-01-07 17:57:33 +0000

    add functions inside object

--- My-app/04-funny-case.js
+++ My-app/04-funny-case.js
@@ -24,30 +24,69 @@ var map = (arr, cb) => {
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-var doubled = map(numbers, element => element*2);
+var doubled = map(numbers, (element) => element * 2);
 console.log(doubled);
 
 console.log(numbers);
 // note this is the original array, and it is unchanged:
 // ----------------------------------------------
 
-
 // 3. filter let's you loop over and return a new array with some elements
-var filter = (arr, cb)=> {
- var result = [];
- for(var index = 0; index < arr.length; index++){
- var currentElement = arr[index];
- if(cb(currentElement, index)){
-   result.push(currentElement);
- }
- }
- return result;
-} ;
+var filter = (arr, cb) => {
+  var result = [];
+  for (var index = 0; index < arr.length; index++) {
+    var currentElement = arr[index];
+    if (cb(currentElement, index)) {
+      result.push(currentElement);
+    }
+  }
+  return result;
+};
 
-var evenNumbers = filter(numbers, currentElement => currentElement %2 === 0);
+var evenNumbers = filter(numbers, (currentElement) => currentElement % 2 === 0);
 
 // Prints `[2, 4, 6, 8, 10]`
 console.log(evenNumbers);
 
 // Note: the original numbers in array is unchanged
 console.log(numbers);
+// ------------------------------------------------------------
+// 4. netflixQueue is an object for managing  your netflix queue:
+// Storing data and managing data:
+var netflixQueue = {
+  queue: [
+    "Matilda",
+    "The Road to El Dordo ",
+    "A Goofy Movie",
+    "Air Bud",
+    "Beethoven",
+    "Chatran",
+    "Pocke Moon",
+    "Two dogs a cat",
+  ],
+  watchMovie: function(){
+    this.queue.pop();
+  },
+  addMovie: function(movie){
+    this.queue.unshift(movie);
+  },
+  printQueue: function(){
+    var list = "";
+    for (var i = this.queue.length - 1; i >= 0; i--) {
+      var currentMovie = this.queue[i];
+      list += this.queue.length - i + "." + currentMovie + "\n";
+    }
+    console.log(list);
+  },
+};
+
+console.log("Printing movie queue!\n");
+netflixQueue.printQueue();
+netflixQueue.watchMovie();
+console.log("Watched a movie!\n")
+console.log("Printing movie queue!\n");
+netflixQueue.printQueue();
+console.log("\n Adding a movie! \n");
+netflixQueue.addMovie("The Lion King");
+console.log("Printing movie queue!\n");
+netflixQueue.printQueue();
