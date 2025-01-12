ec55e16f98c38722dc91f2e1a3b5678ad2fd35ab
Mst <bilkis.naher@yahoo.co.uk>
2025-01-12 14:12:02 +0000

    add template literals method

---
+++ My-app/08-Template-Literals/index.js
@@ -0,0 +1,13 @@
+const arya = {
+    first: "Arya",
+    last: "Stark",
+    origin: "Winterfell",
+    allegiance: "House Stark"
+};
+const greeting = `My name is ${arya.first} !
+I'm loyal to ${arya.allegiance}.`;
+
+console.log(greeting);   //prints : 
+
+// My name is Arya !
+// I'm loyal to House Stark.
